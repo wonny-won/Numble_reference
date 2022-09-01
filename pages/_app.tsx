c2860d460b375bacf423a181d7e23ad8c9f949b3
Jaejune <quswowns345@naver.com>
2022-09-01 12:21:36 +0900

    setting complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import { AppProps } from "next/app";
 import Layout from "../src/components/commons/layout";
 import "../styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: "http://pratice.codebootcamp.co.kr/graphql",
+    cache,
+  });
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
