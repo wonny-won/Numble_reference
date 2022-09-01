7a2993c3af21378030e972c8689c1654db238945
Jaejune <quswowns345@naver.com>
2022-09-01 19:58:10 +0900

    devbranch

--- src/components/units/game/gmaeStyles.ts
+++ src/components/units/game/gmaeStyles.ts
@@ -15,7 +15,7 @@ export const ContainerWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  margin-top: 10px;
+  margin-top: 11px;
 `;
 export const Container = styled.div`
   width: 222px;
