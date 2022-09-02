78b8eba534379d5b7d0b3a17cadb195458e398f7
wonny-won <jaake97@newbizstart.com>
2022-09-02 18:23:42 +0900

    홈 상단부분 완료

--- src/components/units/home/homePresenter.tsx
+++ src/components/units/home/homePresenter.tsx
@@ -1,8 +1,39 @@
-export default function HomePresenter () {
+import * as S from './homeStyled'
 
+export default function HomePresenter (props) {
     return(
+        <>
+        <S.TitleWrap>
+            <S.Title> Update news </S.Title>
+            <S.SubTitle>TODAY STORY</S.SubTitle>
+        </S.TitleWrap>
+        <S.HomeWrap>
+            <div>
+            {
+                props.datalist && props.datalist.map((item:any)=>(
+                    <S.DiaryListWrap key={item?.number}>
+                        <S.Tag src="/images/tag.svg"/>
+                        <div onClick={props.onClickDataList}> {item?.title} </div>
+                    </S.DiaryListWrap>
+                ))
+            }
+            </div>
+            <S.MainInfo>
+                <S.LineWrap>
+                    <S.InfoWrap> 다이어리 <S.Span>0/65</S.Span> </S.InfoWrap>
+                    <S.InfoWrap> 사진첩 <S.Span>0/265</S.Span> </S.InfoWrap>
+                </S.LineWrap>
+                <S.LineWrap>
+                    <S.InfoWrap> 게시판 <S.Span>0/15</S.Span> </S.InfoWrap>
+                    <S.InfoWrap> 방명록 <S.Span>0/15</S.Span> </S.InfoWrap>
+                </S.LineWrap>
+            </S.MainInfo>
+        </S.HomeWrap>
         <div>
-            홈 프레젠터
+            <S.Title style={{marginTop:"25px"}}> 추억의 BGM</S.Title>
+            <div> TODAY CHOICE </div>
         </div>
+        <div></div>
+        </>
     )
 }
---
+++ src/components/units/home/homeStyled.ts
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+
+export const TitleWrap = styled.div`
+    display: flex;
+    width: 240px;
+    align-items: center;
+    padding-bottom:8px;
+    border-bottom: 1.5px solid #999999;
+`
+export const Title = styled.div`
+    font-size: 16px;
+    font-weight: 600;
+    color : #55B2D4;
+`
+export const SubTitle = styled.div`
+    font-size: 8px;
+    margin-left: 20px;
+    padding-top: 4px;
+` 
+export const HomeWrap = styled.div`
+    display: flex;
+`
+export const DiaryListWrap = styled.div`
+    width : 253px;
+    padding: 3px;
+    font-size: 12px;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    &:hover{
+        cursor: pointer;
+    }
+`
+export const Tag = styled.img`
+    margin-right : 10px;
+    width : 35px;
+    height : 14px;
+`
+export const MainInfo = styled.div`
+    width : 194px;
+    height: 60px;
+    border-top: dotted 1.5px #999999;
+    border-bottom: dotted 1.5px #999999;
+    font-size: 8px;
+`
+export const LineWrap = styled.div`
+    width : 194px;
+    display: flex;
+    border-bottom: dotted 1.5px #999999 ;
+    justify-content: space-between;
+    padding-right: 15px;
+`
+export const InfoWrap = styled.div`
+    padding : 3px 0;
+`
+export const Span = styled.span`
+    color : #55B2D4;
+    margin-left : 7px;
+`
