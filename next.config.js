24554f3f06ce93328c996d86d02ceaaa15b9b7bf
Jaejune <quswowns345@naver.com>
2022-09-13 14:43:26 +0900

    reallast

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
   generateBuildId: () => "numble_codecamp",
   exportPathMap: () => ({
     "/game": { page: "/game" },
+    "/diary": { page: "/diary" },
+    "/diary/new": { page: "/diary/new" },
     "/": { page: "/" },
     "/404": { page: "/404" },
   }),
